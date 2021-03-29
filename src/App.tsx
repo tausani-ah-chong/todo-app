43dd1980908c50bca6daa085d1c3f7c702dffb8b
tausani-ah-chong <tausani93@gmail.com>
2021-03-29 19:31:33 +1300

    add text field

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import logo from './logo.svg'
 import './App.css'
 
 function App(): JSX.Element {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+		<div className="container">
+			<header>
+				<h1>Todo App</h1>
 			</header>
+			<main>
+				<form action="" className="todo">
+					<input type="text" placeholder="What do you want to achieve?" />
+				</form>
+			</main>
 		</div>
 	)
 }
