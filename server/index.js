2a0432acad359790db5da5b2967c1ff2eda52c5b
tausani-ah-chong <tausani93@gmail.com>
2021-04-01 13:27:34 +1300

    add get route, select all todos query

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const server = express()
 
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server listening on port', port)
+})
+
+server.use(express.json())
+
+const apiRoute = "/api/v1"
+
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
@@ -10,11 +21,7 @@ const db = mysql.createConnection({
   database: 'todoList'
 })
 
-server.use(express.json())
-
-const port = process.env.PORT || 5000
-
-server.post('/create', (req, res) => {
+server.post(apiRoute, (req, res) => {
   const todoText = req.body.text
   db.query(
     "INSERT INTO todos (text) VALUES (?)",
@@ -30,7 +37,13 @@ server.post('/create', (req, res) => {
   )
 })
 
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server listening on port', port)
+server.get(apiRoute, (req, res) => {
+  db.query("SELECT * FROM todos", (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(result)
+      console.log("Success!");
+    }
+  })
 })
